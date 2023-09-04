1a7c44c59c63781b4de28fd113e5e5652e6f9051
root <anthony.n.ndegwa@gmail.com>
2023-09-03 23:28:50 -0700

    Import event-loader

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,7 @@
 import mongodb from 'mongodb';
 // eslint-disable-next-line no-unused-vars
 import Collection from 'mongodb/lib/collection';
+import envLoader from './env_loader';
 
 /**
  * Represents a MongoDB client.
